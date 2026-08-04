55280f64e15d488fd472a6b27b5dbc9968b61a37
yuanzhe-jia <yuanzhe.dev@gmail.com>
2026-08-04 16:55:43 +0800

    fix google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -57,11 +56,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7905191864325907"
           crossOrigin="anonymous"
-          strategy="lazyOnload"
         />
       </head>
       <body className="font-sans antialiased min-h-screen flex flex-col">
