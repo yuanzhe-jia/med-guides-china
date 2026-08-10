ad2755dac93b958b84f00636f4c54b5f1ef1a21f
yuanzhe-jia <yuanzhe.dev@gmail.com>
2026-08-10 12:10:28 +0800

    update site ownership verification methods

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7905191864325907" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7905191864325907"
